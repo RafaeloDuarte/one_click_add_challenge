6810402b6fc062ae8f877c749aee243660f39acc
RafaeloDuarte <rafaeloduarte@gmail.com>
2025-01-27 15:32:17 -0300

    fix: Corrige infra

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import { app } from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.listen({
+  port: PORT, hostname: "0.0.0.0", callback: () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  }
 });
